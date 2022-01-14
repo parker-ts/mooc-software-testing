6c0a9fa475dd7e26091a177462e5e7d9c7869fdb
ParkerThomasScott <scottpt@uci.edu>
2022-01-13 18:14:19 -0800

    Update GettingStartedTest.java

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -1,13 +1,21 @@
 package tudelft.gettingstarted;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GettingStartedTest {
 
+    GettingStarted g;
+
+    @BeforeEach
+    public void setUp() {
+        g = new GettingStarted();
+    }
+
     @Test
     public void addFiveTo20() {
-        int result = new GettingStarted().addFive(20);
+        int result = g.addFive(20);
         Assertions.assertEquals(25,result);
     }
 
@@ -15,31 +23,31 @@ public class GettingStartedTest {
 
     @Test
     public void addFiveToZero() {
-        int result = new GettingStarted().addFive(0);
+        int result = g.addFive(0);
         Assertions.assertEquals(5, result);
     }
 
     @Test
     public void addFiveToMinus20() {
-        int result = new GettingStarted().addFive(-20);
+        int result = g.addFive(-20);
         Assertions.assertEquals(-15,result);
     }
 
     @Test
     public void addFiveToMinus1000() {
-        int result = new GettingStarted().addFive(-1000);
+        int result = g.addFive(-1000);
         Assertions.assertEquals(-995,result);
     }
 
     @Test
     public void addFiveToMinusOne() {
-        int result = new GettingStarted().addFive(-1);
+        int result = g.addFive(-1);
         Assertions.assertEquals(4,result);
     }
 
     @Test
     public void addFiveToMinusFive() {
-        int result = new GettingStarted().addFive(-5);
+        int result = g.addFive(-5);
         Assertions.assertEquals(0,result);
     }
 }
